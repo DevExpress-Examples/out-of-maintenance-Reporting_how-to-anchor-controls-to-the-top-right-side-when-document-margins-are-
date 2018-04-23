1208fb2827e9bd273a468b78a5e556ab2f8e413a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to anchor controls to the top-right side when document margins are changed in the Print Preview
+
+
+<p>This example illustrates how to make all reports "page-sensitive" (page size, margin sizes, etc.), and to anchor controls to top and left sides like regular WinForms controls do. To accomplish this task, it's required to handle the <strong>PrintingSystem.AfterMarginsChange</strong> and <strong>PrintingSystem.PageSettingsChanged</strong> events, and change the width and location of controls according to the current page settings.<br /><br />See also: <a href="https://www.devexpress.com/Support/Center/p/T272219">How to dynamically size an image to make it fit into the entire page client area</a></p>
+
+
+<h3>Description</h3>
+
+<p>This example uses the <strong>HundredthsOfAnInch</strong> as the <a href="https://documentation.devexpress.com/#XtraReports/DevExpressXtraReportsUIXtraReport_ReportUnittopic">ReportUnit</a>. Using&nbsp;<strong>TenthsOfAMillimeter</strong> requires to additionally convert the values in the <strong>ChangeReportSettings</strong> method.</p>
+
+<br/>
+
+
---
+++ VB/XtraReport1.Designer.vb
@@ -0,0 +1,94 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace WindowsApplication1
+	Partial Public Class XtraReport1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(XtraReport1))
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.PageHeader = New DevExpress.XtraReports.UI.PageHeaderBand()
+			Me.xrPictureBox1 = New DevExpress.XtraReports.UI.XRPictureBox()
+			Me.PageFooter = New DevExpress.XtraReports.UI.PageFooterBand()
+			Me.topMarginBand1 = New DevExpress.XtraReports.UI.TopMarginBand()
+			Me.bottomMarginBand1 = New DevExpress.XtraReports.UI.BottomMarginBand()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' Detail
+			' 
+			Me.Detail.HeightF = 25F
+			Me.Detail.Name = "Detail"
+			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' PageHeader
+			' 
+			Me.PageHeader.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrPictureBox1})
+			Me.PageHeader.HeightF = 142F
+			Me.PageHeader.Name = "PageHeader"
+			Me.PageHeader.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.PageHeader.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' xrPictureBox1
+			' 
+			Me.xrPictureBox1.Image = (CType(resources.GetObject("xrPictureBox1.Image"), System.Drawing.Image))
+			Me.xrPictureBox1.LocationFloat = New DevExpress.Utils.PointFloat(583F, 0F)
+			Me.xrPictureBox1.Name = "xrPictureBox1"
+			Me.xrPictureBox1.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.xrPictureBox1.SizeF = New System.Drawing.SizeF(95F, 92F)
+			Me.xrPictureBox1.Sizing = DevExpress.XtraPrinting.ImageSizeMode.ZoomImage
+			' 
+			' PageFooter
+			' 
+			Me.PageFooter.HeightF = 30F
+			Me.PageFooter.Name = "PageFooter"
+			Me.PageFooter.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.PageFooter.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' topMarginBand1
+			' 
+			Me.topMarginBand1.Name = "topMarginBand1"
+			' 
+			' bottomMarginBand1
+			' 
+			Me.bottomMarginBand1.Name = "bottomMarginBand1"
+			' 
+			' XtraReport1
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.PageHeader, Me.PageFooter, Me.topMarginBand1, Me.bottomMarginBand1})
+			Me.Margins = New System.Drawing.Printing.Margins(100, 72, 100, 100)
+			Me.Version = "12.2"
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private Detail As DevExpress.XtraReports.UI.DetailBand
+		Private PageHeader As DevExpress.XtraReports.UI.PageHeaderBand
+		Private PageFooter As DevExpress.XtraReports.UI.PageFooterBand
+		Private xrPictureBox1 As DevExpress.XtraReports.UI.XRPictureBox
+		Private topMarginBand1 As DevExpress.XtraReports.UI.TopMarginBand
+		Private bottomMarginBand1 As DevExpress.XtraReports.UI.BottomMarginBand
+	End Class
+End Namespace
