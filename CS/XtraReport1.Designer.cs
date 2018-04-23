398c38bf70fa3bb9519da6ab2d7935d58eaeb5f1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/XtraReport1.Designer.cs
@@ -0,0 +1,76 @@
+namespace WindowsApplication1 {
+    partial class XtraReport1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XtraReport1));
+            this.Detail = new DevExpress.XtraReports.UI.DetailBand();
+            this.PageHeader = new DevExpress.XtraReports.UI.PageHeaderBand();
+            this.xrPictureBox1 = new DevExpress.XtraReports.UI.XRPictureBox();
+            this.PageFooter = new DevExpress.XtraReports.UI.PageFooterBand();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // Detail
+            // 
+            this.Detail.Height = 25;
+            this.Detail.Name = "Detail";
+            // 
+            // PageHeader
+            // 
+            this.PageHeader.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.xrPictureBox1});
+            this.PageHeader.Height = 142;
+            this.PageHeader.Name = "PageHeader";
+            // 
+            // xrPictureBox1
+            // 
+            this.xrPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("xrPictureBox1.Image")));
+            this.xrPictureBox1.Location = new System.Drawing.Point(583, 0);
+            this.xrPictureBox1.Name = "xrPictureBox1";
+            this.xrPictureBox1.Size = new System.Drawing.Size(95, 92);
+            this.xrPictureBox1.Sizing = DevExpress.XtraPrinting.ImageSizeMode.ZoomImage;
+            // 
+            // PageFooter
+            // 
+            this.PageFooter.Height = 30;
+            this.PageFooter.Name = "PageFooter";
+            // 
+            // XtraReport1
+            // 
+            this.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] {
+            this.Detail,
+            this.PageHeader,
+            this.PageFooter});
+            this.Margins = new System.Drawing.Printing.Margins(100, 72, 100, 100);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraReports.UI.DetailBand Detail;
+        private DevExpress.XtraReports.UI.PageHeaderBand PageHeader;
+        private DevExpress.XtraReports.UI.PageFooterBand PageFooter;
+        private DevExpress.XtraReports.UI.XRPictureBox xrPictureBox1;
+    }
+}
---
+++ VB/XtraReport1.vb
@@ -0,0 +1,48 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Drawing
+Imports DevExpress.XtraReports.UI
+Imports DevExpress.XtraPrinting
+
+Namespace WindowsApplication1
+	Partial Public Class XtraReport1
+		Inherits XtraReport
+		Public Sub New()
+			InitializeComponent()
+			AddHandler PrintingSystem.AfterMarginsChange, AddressOf PrintingSystem_AfterMarginsChange
+			AddHandler PrintingSystem.PageSettingsChanged, AddressOf PrintingSystem_PageSettingsChanged
+		End Sub
+
+		Private Sub PrintingSystem_PageSettingsChanged(ByVal sender As Object, ByVal e As EventArgs)
+			ChangeReportSettings(sender)
+		End Sub
+
+		Private Sub PrintingSystem_AfterMarginsChange(ByVal sender As Object, ByVal e As MarginsChangeEventArgs)
+			ChangeReportSettings(sender)
+		End Sub
+
+		Private Sub ChangeReportSettings(ByVal sender As Object)
+			Dim ps As PrintingSystemBase = TryCast(sender, PrintingSystemBase)
+			Dim isLocationChanged As Boolean = False
+			Dim newPageWidth As Integer = ps.PageBounds.Width - ps.PageMargins.Left - ps.PageMargins.Right
+			Dim currentPageWidth As Integer = Me.PageWidth - Me.Margins.Left - Me.Margins.Right
+			Dim shift As Integer = currentPageWidth - newPageWidth
+			For Each _band As Band In MyBase.Bands
+				For Each _control As XRControl In _band.Controls
+					isLocationChanged = True
+					_control.Location = New Point((_control.Location.X - shift), _control.Location.Y)
+				Next _control
+			Next _band
+			If isLocationChanged = True Then
+				Margins.Top = ps.PageMargins.Top
+				Margins.Bottom = ps.PageMargins.Bottom
+				Margins.Left = ps.PageMargins.Left
+				Margins.Right = ps.PageMargins.Right
+				PaperKind = ps.PageSettings.PaperKind
+				PaperName = ps.PageSettings.PaperName
+				Landscape = ps.PageSettings.Landscape
+				CreateDocument()
+			End If
+		End Sub
+	End Class
+End Namespace
